698ced756f7710da376f1b4ad9c25154ba47215e
AlkanTuncer <alkantuncer53@hotmail.com>
2022-08-17 21:26:53 +0300

    Cucumber - Runner Class

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "stepdefinitions",
         tags = "@first", // feature içinde tag vererek istediğimiz testleri çalıştırabiliriz.
-        dryRun = false)  // feature'daki metotları almak için 'true' kullanabiliriz.
+        dryRun = false)  // feature'daki metotları almak için 'true' kullanabiliriz. 'true' iken testleri calıstırmaz.
+                         // 'false' iken belirtilen dosyadaki bütün testleri calıstırır. 'tags' verirsek sadece onları calıstırır.
 
 public class Runner { }
