b7261dc9d3fa322c96b3fb0750d69479672c9990
AlkanTuncer <alkantuncer53@hotmail.com>
2022-08-19 18:52:01 +0300

    Cucumber - Runner Class Text

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -11,4 +11,17 @@ import org.junit.runner.RunWith;
         dryRun = false)  // feature'daki metotları almak için 'true' kullanabiliriz. 'true' iken testleri calıstırmaz.
                          // 'false' iken belirtilen dosyadaki bütün testleri calıstırır. 'tags' verirsek sadece onları calıstırır.
 
-public class Runner { }
+public class Runner {
+
+    // Runner class'inin body'sine hic bir kod yazmiyoruz
+    // bu class icin onemli olan kullanacagimiz 2 adet notasyon
+
+    // dryRun=false yazildiginda belirlenen tag'la etiketlenen tum scenario'lari sirasiyla calistirir
+    // dryRun=true dedigimizde ise kodlari calistirmadan eksik stepler olup olmadigini kontrol eder ve varsa bize eksik stepleri rapor eder
+
+    // features ve glue olarak spesifik bir class ve ya feature dosyasini degil tum klasor ve package'i sectik
+    // dolayisiyla stepdefinitions package'i icerisinde hangi class'da olursa olsun isimize yarayan bir stepdefinition varsa rahatlikla kullanabiliriz
+
+
+
+}
